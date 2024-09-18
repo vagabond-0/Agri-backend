7da4c74ce70ff832aca341aae023974e82e2d9f7
amalendu123 <220229@tkmce.ac.in>
2024-09-18 10:30:31 +0530

    Initial commit

---
+++ src/main/java/com/example/Agri/AgriApplication.java
@@ -0,0 +1,13 @@
+package com.example.Agri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Agri/AgriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Agri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
